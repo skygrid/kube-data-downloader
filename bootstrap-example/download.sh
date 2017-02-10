f861cebcc099484aad1f5ff0133186781600ff01
Sasha Baranov <sashab1@yandex-team.ru>
2017-02-10 17:30:24 +0300

    Fixes for minikube

--- bootstrap-example/download.sh
+++ bootstrap-example/download.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# For minikube you would need to:
+# minikube ssh
+# sudo umount /Users
+# sudo mount -t vboxsf -o rw,nodev,relatime,suid,dmode=600,fmode=600 Users /Users
+
+
 grid-proxy-init -rfc -debug -cert grid_proxy -key grid_proxy
-./download-script input.json /input
+python download-script input.json /input
